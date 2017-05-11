a11746b346aeb8da5a8c0a71d5409b749b57e8c4
Leonardo <f.leobrito@gmail.com>
2017-05-11 02:27:36 -0300

    Pokedex completa

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Leonardo Brito on 29/04/17.
+//  Copyright © 2017 Leonardo Brito. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co/"
+let URL_POKEMON = "api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Leonardo Brito on 19/04/17.
+//  Copyright © 2017 Leonardo Brito. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
